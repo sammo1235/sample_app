e38486303a18ff58c2dcf8c234b40478725ddcbb
sammo1235 <samrhysperry93@hotmail.co.uk>
2019-04-16 23:34:41 +0100

    friendly forwarding not working yet

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -44,4 +44,14 @@ module SessionsHelper
     session.delete(:user_id)
     @current_user = nil
   end
+
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  def store_location
+    session[:forwaring_url] = request.original_url if request.get?
+  end
+
 end
