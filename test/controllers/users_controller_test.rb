319d2f38e666e5fbc3613f691eef7ee6d4298741
sammo1235 <samrhysperry93@hotmail.co.uk>
2019-04-18 16:33:14 +0100

    add admin can delete users and tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -51,6 +51,21 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     patch user_path(@other_user), params: { user: { password: 'password',
       password_confirmation: 'password',
       admin: true}}
-      assert_not @other_user.admin.true?
+    assert_not @other_user.admin?, "user is not an admin"
+  end
+
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
     end
+    assert_redirected_to login_url
+    end
+
+  test "should redirect destroy when logged in as a non-admin" do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to root_url
+  end
 end
---
+++ test/integration/users_index_test.rb
@@ -0,0 +1,31 @@
+require 'test_helper'
+
+class UsersIndexTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @admin     = users(:michael)
+    @non_admin = users(:archer)
+  end
+
+  test "index as non-admin" do
+    log_in_as(@non_admin)
+    get users_path
+    assert_select 'a', text: 'delete', count: 0
+  end
+
+  test "index as admin" do
+    log_in_as(@admin)
+    get users_path
+    assert_template 'users/index'
+    all_users = User.all
+    all_users.each do |user|
+      unless user == @admin
+        assert_select 'a[href=?]', user_path(user), text: 'delete'
+      end
+    end
+    assert_difference 'User.count', -1 do
+    delete user_path(@non_admin)
+    end
+  end
+
+end
