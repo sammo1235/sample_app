53b640663d34c20113aff1748d29766957ae5410
sammo1235 <samrhysperry93@hotmail.co.uk>
2019-05-07 21:12:52 +0100

    add post delete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -53,14 +53,6 @@ class UsersController < ApplicationController
         :password_confirmation)
     end
 
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
-
     def admin_user
       redirect_to(root_url) unless current_user.admin?
     end
